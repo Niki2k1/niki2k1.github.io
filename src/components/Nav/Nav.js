792ae66312ca1ea45cf4804ba573f5328fdba56f
Niklas Lausch <support@niki2k1.de>
2019-12-09 08:56:01 +0100

    Updated Nav
    Fixed sec-bug

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -8,10 +8,10 @@ class Nav extends React.Component {
             <a href="/"><img width="110px"  src="/logo512.png" alt="Logo"/></a>
             <h1>Niklas Lausch</h1>
             <div className="icons">
-                <a href="http://twitter.niki2k1.de" target="_blank"><img src="/icons/twitter.png" alt="twitter"/></a>
-                <a href="http://discord.niki2k1.de" target="_blank"><img src="/icons/discord.png" alt="discord"/></a>
-                <a href="http://yt.niki2k1.de" target="_blank"><img src="/icons/youtube.png" alt="youtube"/></a>
-                <a href="https://www.xing.com/profile/Niklas_Lausch" target="_blank"><img src="/icons/xing.png" alt="xing"/></a>
+                <a href="http://twitter.niki2k1.de" target="_blank" rel="noopener noreferrer"><img src="/icons/twitter.png" alt="twitter"/></a>
+                <a href="http://discord.niki2k1.de" target="_blank" rel="noopener noreferrer"><img src="/icons/discord.png" alt="discord"/></a>
+                <a href="http://yt.niki2k1.de" target="_blank" rel="noopener noreferrer"><img src="/icons/youtube.png" alt="youtube"/></a>
+                <a href="https://www.xing.com/profile/Niklas_Lausch" target="_blank" rel="noopener noreferrer"><img src="/icons/xing.png" alt="xing"/></a>
             </div>
         </div>
     }
