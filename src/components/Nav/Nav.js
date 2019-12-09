90664d22e742473e75e48c3aa22152e723e0e702
Niklas Lausch <support@niki2k1.de>
2019-12-09 08:54:03 +0100

    Updated Nav
    Added Links to Header Icons

--- src/components/Nav/Nav.js
+++ src/components/Nav/Nav.js
@@ -5,13 +5,13 @@ class Nav extends React.Component {
 
     render() {
         return <div className="nav">
-            <img width="110px"  src="/logo512.png" alt="Logo"></img>
+            <a href="/"><img width="110px"  src="/logo512.png" alt="Logo"/></a>
             <h1>Niklas Lausch</h1>
             <div className="icons">
-                <img src="/icons/twitter.png" alt="twitter"></img>
-                <img src="/icons/discord.png" alt="discord"></img>
-                <img src="/icons/youtube.png" alt="youtube"></img>
-                <img src="/icons/xing.png" alt="xing"></img>
+                <a href="http://twitter.niki2k1.de" target="_blank"><img src="/icons/twitter.png" alt="twitter"/></a>
+                <a href="http://discord.niki2k1.de" target="_blank"><img src="/icons/discord.png" alt="discord"/></a>
+                <a href="http://yt.niki2k1.de" target="_blank"><img src="/icons/youtube.png" alt="youtube"/></a>
+                <a href="https://www.xing.com/profile/Niklas_Lausch" target="_blank"><img src="/icons/xing.png" alt="xing"/></a>
             </div>
         </div>
     }
