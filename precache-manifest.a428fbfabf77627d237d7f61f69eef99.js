544daf057aaf5d19806b96a116f5a5243b065834
Niki2k1 <Niki2k1@users.noreply.github.com>
2020-02-10 21:28:41 +0000

    Deploy to GitHub pages

---
+++ precache-manifest.a428fbfabf77627d237d7f61f69eef99.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d83717aad57c408c9f00fd41d264142a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e42da42431534fb00774",
+    "url": "/static/css/main.c28387f4.chunk.css"
+  },
+  {
+    "revision": "224af9a7a821045eadd3",
+    "url": "/static/js/2.b23591f3.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.b23591f3.chunk.js.LICENSE"
+  },
+  {
+    "revision": "e42da42431534fb00774",
+    "url": "/static/js/main.bb63dc81.chunk.js"
+  },
+  {
+    "revision": "28f50c77859d4f294b32",
+    "url": "/static/js/runtime-main.5dddde80.js"
+  }
+]);
